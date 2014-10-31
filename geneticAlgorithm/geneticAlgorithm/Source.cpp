9d40256c1447d7c7ee23f6f3f76f12d3f52f5c0b
Nicklaus Benedict <nrb1001@sru.edu>
2014-10-31 11:46:01 -0400

    Update Source.cpp

--- geneticAlgorithm/geneticAlgorithm/Source.cpp
+++ geneticAlgorithm/geneticAlgorithm/Source.cpp
@@ -1,7 +1,9 @@
 #include "City.h"			//Defines City Class
 #include "Route.h"			//Defines Route Class
-#include "GeneticFuncs.h"	//Holds GA Functions
+//#include "GeneticFuncs.h"	//Holds GA Functions
 #include <iostream> 
+#include <fstream>
+#include <string.h>
 
 /*For purposes of TSP 
 	City = a node on a graph 
@@ -9,30 +11,47 @@
 */
 
 int main() {
-	
-	//set up city array
-	//generate routes
 
-	//while loop
-	//pair best parents
-	//cross parents
-	//check for best children
-	//half best parents, half best children
+//create city array
+	ifstream myReadFile;
+	string nextData;
+	City theCityArray[29];
 
-	//loop ends when 50 successive generations do not generate better child.
-	
-	City a = City(0.5, 120);
-	a.setName("45");
-	Route r = Route();
-	r.addCity(a);
-	r.addCity(a);
-	City *ary = r.getCities();
-	int size = r.getNumCities();
-	for (int i = 0; i < size; i++){
-		cout << "City name = " << ary[i].getName() << "\n";
+	int col = 1;
+	int i = 0;
+
+	myReadFile.open("29CitiesData.txt");
+
+	if(myReadFile.is_open()){
+		while(myReadFile >> nextData){
+			if(col == 1){
+				theCityArray[i].setId(atoi(nextData.c_str()));
+				cout << nextData << endl;
+				col++;
+			}
+			else if(col == 2){
+				theCityArray[i].setX(atof(nextData.c_str()));
+				cout << nextData << endl;
+				col++;
+			}
+			else if(col == 3){
+				theCityArray[i].setY(atof(nextData.c_str()));
+				cout << nextData << endl;
+				col=1;
+				i++;
+			}
+			else
+				cout << "YA DONE FUCKED UP M8!";
+		}//while has next data
+	}//if file open
+
+	for(int i = 0; i < 29; i++)
+	{
+		cout << theCityArray[i].getId() << endl;
 	}
+
 	//cout << "City name = " << r.getCityAt(1).getName() << "\n";
-	testFunc();
+	//testFunc();
 	system("Pause");
 	return 0;
 }
