94b719c84404a07092e2aa590db4d58e949e35d5
zgp1001 <zgp1001@sru.edu>
2014-11-12 15:03:55 -0500

    Set up trial merge sort
    Zach told me to

--- geneticAlgorithm/geneticAlgorithm/Source.cpp
+++ geneticAlgorithm/geneticAlgorithm/Source.cpp
@@ -15,6 +15,69 @@ const int NUM_CITIES = 29;
 	Route = a tour of Cities listed in traversal order (linked list, essentially) 
 */
 
+void sortRouteArray();
+/*
+ * C++ Program to Implement Merge Sort
+ */
+#include <iostream>
+using namespace std;
+#include <conio.h>
+
+void merge(Route *,int, int , int );
+void mergesort(Route *a, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid=(low+high)/2;
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,high,mid);
+    }
+    return;
+}
+void merge(int *a, int low, int high, int mid)
+{
+    int i, j, k;
+	Route c = new Route[high-low];
+    i = low;
+    k = low;
+    j = mid + 1;
+	
+    while (i <= mid && j <= high)
+    {
+        if (a[i].getDistance() < a[j].getDistance())
+        {
+            c[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            c[k] = a[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid)
+    {
+        c[k] = a[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        c[k] = a[j];
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)
+    {
+        a[i] = c[i];
+    }
+	delete []c;
+}
+
 int main() {
 
 
